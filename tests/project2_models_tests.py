06599b6a6764291307941969ae9a0c08f91b14a1
jeroyang <jeroyang@gmail.com>
2012-09-04 11:01:47 +0800

    Successful test run

--- tests/project2_models_tests.py
+++ tests/project2_models_tests.py
@@ -1,3 +1,9 @@
 from nose.tools import *
 from project2.models import *
 
+def test_dictionary_compressor():
+    tokens = ['automata', 'automate', 'automatic', 'automation']
+    assert_equal(DictionaryCompressor(tokens), '8automate*a1\te2\tic3\tion')
+    
+
+    
